0a98dbe183461f751685db268c2667f19d7a22c6
JnJuste <jeanjusteirakoze@gmail.com>
2024-07-19 19:18:05 +0200

    Project Initialization

---
+++ src/test/java/com/jnjuste/hospitalms/HospitalMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnjuste.hospitalms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
