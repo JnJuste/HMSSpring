472c024d9b470eed392e23d8708e735eac90c17e
JnJuste <jeanjusteirakoze@gmail.com>
2024-07-27 13:43:03 +0200

    Password Generator

--- src/main/java/com/jnjuste/hospitalms/utils/PasswordGenerator.java
+++ src/main/java/com/jnjuste/hospitalms/utils/PasswordGenerator.java
@@ -3,14 +3,37 @@ package com.jnjuste.hospitalms.utils;
 import java.security.SecureRandom;
 
 public class PasswordGenerator {
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    private static final String ALL_CHARACTERS = UPPERCASE + LOWERCASE + DIGITS + SPECIAL_CHARACTERS;
+
     private static final SecureRandom RANDOM = new SecureRandom();
 
     public static String generateRandomPassword(int length) {
         StringBuilder password = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            password.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+
+        // Ensure at least one character from each category
+        password.append(UPPERCASE.charAt(RANDOM.nextInt(UPPERCASE.length())));
+        password.append(LOWERCASE.charAt(RANDOM.nextInt(LOWERCASE.length())));
+        password.append(DIGITS.charAt(RANDOM.nextInt(DIGITS.length())));
+        password.append(SPECIAL_CHARACTERS.charAt(RANDOM.nextInt(SPECIAL_CHARACTERS.length())));
+
+        // Fill the rest of the password with random characters
+        for (int i = 4; i < length; i++) {
+            password.append(ALL_CHARACTERS.charAt(RANDOM.nextInt(ALL_CHARACTERS.length())));
         }
-        return password.toString();
+
+        // Shuffle the password
+        char[] passwordArray = password.toString().toCharArray();
+        for (int i = passwordArray.length - 1; i > 0; i--) {
+            int index = RANDOM.nextInt(i + 1);
+            char temp = passwordArray[index];
+            passwordArray[index] = passwordArray[i];
+            passwordArray[i] = temp;
+        }
+
+        return new String(passwordArray);
     }
 }
